ec2b077c097170d87676f3c0bfd81cc88bbf3c68
Jonathan Nguyen <awsome911@gmail.com>
2018-11-19 00:47:06 -0800

    Fixed incorrect use of read_raw in main()

--- Libraries/main.cpp
+++ Libraries/main.cpp
@@ -5,17 +5,17 @@
 
 const float pi = 3.14159265359;
 
-void bias_calc(MPU6050 mpu, float bias[6])
+void bias_calc(MPU6050 *mpu, float bias[6])
 {
-    float* data[6];
+    float data[6];
 
     for(int i =0; i < 20; i++)
     {
-        mpu.read_raw(data[0],data[1],data[2],data[3],data[4],data[5]);
+        mpu->read_raw(&data[0],&data[1],&data[2],&data[3],&data[4],&data[5]);
 
         //sum errors and average
         for(int j = 0; j < 6; j++)
-            bias[j] += (*data[j]/20);
+            bias[j] += (data[j]/20);
     }
 
     //expect ax to be 1 upwards
@@ -36,20 +36,20 @@ int main()
     float bias[6];
 
     //get the biases
-    bias_calc(mpu, bias);
+    bias_calc(&mpu, bias);
 
     while(1)
     {
         //get the raw data
-        float* data[6];
-        mpu.read_raw(data[0],data[1],data[2],data[3],data[4],data[5]);
+        float data[6];
+        mpu.read_raw(&data[0], &data[1], &data[2], &data[3], &data[4], &data[5]);
 
         //account for bias and normalize the vector
-        vector orientation = {*data[0] - bias[0], *data[1] - bias[1], *data[2] - bias[2]};
+        vector orientation = {data[3] - bias[3], data[4] - bias[4], data[5] - bias[5]};
         vector norm_orientation;
         vector_normalize(&orientation, &norm_orientation);
 
-        pc.printf("%f %f %f\r\n", norm_orientation.x, norm_orientation.y, norm_orientation.z);
+        pc.printf("<accel>: %f, %f, %f\r\n", norm_orientation.x, norm_orientation.y, norm_orientation.z);
         wait(0.1);
     }
 }
